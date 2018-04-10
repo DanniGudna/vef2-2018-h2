92be02dc48822c3c085b2a800c6caaac54d4a518
geirgardarsson <geg42@hi.is>
2018-04-10 15:57:36 +0000

    Bætti við implementation á login og signup action

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -25,17 +25,17 @@ function requestLogin() {
   }
 }
 
-function loginSuccess(user) {
+function loginSuccess(token) {
   return {
     type: LOGIN_SUCCESS,
     isFetching: false,
     isAuthenticated: true,
-    user,
+    token,
     message: null,
   }
 }
 
-function loginFail(message) {
+function loginError(message) {
   return {
     type: LOGIN_FAILURE,
     isFetching: false,
@@ -54,16 +54,78 @@ function logout() {
 }
 
 
+function signupRequest() {
+  return {
+    type: SIGNUP_REQUEST,
+    isFetching: true,
+    message: null,
+  }
+}
+
+function signupSuccess(user) {
+  return {
+    type: SIGNUP_SUCCESS,
+    isFetching: false,
+    user,
+    message: null,
+  }
+}
+
+function signupError(message) {
+  return {
+    type: SIGNUP_FAILURE,
+    isFetching: false,
+    message: null,
+  }
+}
+
+
 export const loginUser = (username, password) => {
   return async (dispatch) => {
     dispatch(requestLogin());
 
-    // const endpoint = 
+    const endpoint = '/login';
+
+    let token;
+
+    try {
+      token = await api.post(endpoint, {username, password});
+    } catch (error) {
+      dispatch(loginError(error));
+    }
+
+    console.info(token);
+
+    if (!token) {
+      dispatch(loginError('Oh no!'));
+    }
+
+    dispatch(loginSuccess(token));
   }
 }
 
-export const signup = () => {
+export const signup = (username, password, name) => {
+  return async (dispatch) => {
+    dispatch(signupRequest());
 
+    const endpoint = '/register';
+
+    let user;
+
+    try {
+      user = await api.post(endpoint, {username, password, name})
+    } catch (error) {
+      dispatch(signupError(error));
+    }
+
+    console.info(user);
+
+    if (!user) {
+      dispatch(signupError('Oh no!'));
+    }
+
+    dispatch(signupSuccess(user));
+  }
 }
 
 export const logoutUser = () => {
--- src/reducers/auth.js
+++ src/reducers/auth.js
@@ -1,17 +1,73 @@
 import {
   LOGIN_REQUEST,
-  /* todo fleiri actions */
+  LOGIN_SUCCESS,
+  LOGIN_FAILURE,
+  LOGIN_LOGOUT,
+  SIGNUP_REQUEST,
+  SIGNUP_SUCCESS,
+  SIGNUP_FAILURE,
 } from '../actions/auth';
 
 const initialState = {
+  isFetching: false,
+  isAuthenticated: false,
+  message: null,
+  token: null,
+  user: null,
 };
 
 export default (state = initialState, action) => {
+
   switch (action.type) {
-
-    /* todo setja upp reducer */
-
-    default:
-      return state;
+    case LOGIN_REQUEST:
+      return {
+        ...state,
+        isFetching: action.isFetching,
+        isAuthenticated: action.isAuthenticated,
+        message: action.message,
+      };
+    case LOGIN_SUCCESS:
+      return {
+        ...state,
+        isFetching: action.isFetching,
+        isAuthenticated: action.isAuthenticated,
+        token: action.token,
+        message: action.message,
+      }
+    case LOGIN_FAILURE:
+      return {
+        ...state,
+        isFetching: action.isFetching,
+        isAuthenticated: action.isAuthenticated,
+        message: action.message,
+      }
+    case LOGIN_LOGOUT:
+      return {
+        ...state,
+        isFetching: action.isFetching,
+        isAuthenticated: action.isAuthenticated,
+        token: action.token,
+      }
+    case SIGNUP_REQUEST:
+      return {
+        ...state,
+        isFetching: action.isFetching,
+        message: action.message,        
+      }
+    case SIGNUP_SUCCESS:
+      return {
+        ...state,
+        isFetching: action.isFetching,
+        user: action.user,
+        message: action.message,
+      }
+    case SIGNUP_FAILURE:
+      return {
+        ...state,
+        isFetching: action.isFetching,
+        message: action.message,
+      }
+  default:
+    return state;
   }
 };
