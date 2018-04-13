1b38230878b6388c87085f3cc337af933c553682
geirgardarsson <geg42@hi.is>
2018-04-13 12:07:41 +0000

    Bætti við isFetching í authentication í App.js

--- src/routes/profile/Profile.js
+++ src/routes/profile/Profile.js
@@ -1,15 +1,64 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
-export default class Profile extends Component {
+import Button from '../../components/button';
+import Field from '../../components/field';
+
+class Profile extends Component {
+  state = {
+    name: '',
+    password: '',
+    passwordAgain: '',
+    img: null,
+  }
 
   render() {
     return (
       <div>
         <h1>Upplýsingar</h1>
-        <div>
-          upload photo
-        </div>
+        <form>
+          <div>
+            <input type="file" />
+            <Button>Uppfæra mynd</Button>
+          </div>
+        </form>
+        <form>
+          <Field
+            name="name"
+            label="Nafn"
+            value=""
+            type="text"
+            onChange={null}
+          />
+          <Button>Uppfæra nafn</Button>
+        </form>
+        <form>
+          <Field
+            name="password"
+            label="Lykilorð"
+            value=""
+            type="password"
+            onChange={null}
+          />
+          <Field
+            name="password-again"
+            label="Lykilorð, aftur"
+            value=""
+            type="password"
+            onChange={null}
+          />
+          <Button>Uppfæra nafn</Button>
+        </form>
+        <h1>Lesnar bækur</h1>
       </div>
     );
   }
 }
+
+const mapStateToProps = (state) => {
+  return {
+    ...state,
+  }
+}
+
+export default connect(mapStateToProps)(Profile);
